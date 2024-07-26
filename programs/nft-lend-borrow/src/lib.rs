bf1ca98a0e279476e87eddb4722479a647cbb957
hughiestar <KuklerHugron496@gmail.com>
2024-07-26 08:37:03 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("Fe5dwRyJ88mi8UjFGCRiFaUjYgHCVFrzCh4dnezTZHcb");
 
 #[program]
 pub mod nft_lend_borrow {
